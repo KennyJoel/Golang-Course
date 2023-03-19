8b05bc9815a2fac9d98c0ded17886ec8ffd4aa74
KURAGAYALA KENNY JOEL <50044069+KennyJoel@users.noreply.github.com>
2023-03-19 10:16:35 +0530

    1.go

--- 1.go
+++ 1.go
@@ -7,8 +7,15 @@ import (
 
 func main() {
 	numProblems := 5 // change the number of problems here
-	fmt.Printf("I will solve %d %s:\n", numProblems)
+	fmt.Printf("I will solve %d %s:\n", numProblems, pluralize(numProblems, "problem", "problems"))
 	for i := 1; i <= numProblems; i++ {
 		fmt.Printf("Problem %d: %d + %d = _____\n", i, rand.Intn(10), rand.Intn(10))
 	}
+}
+
+func pluralize(num int, singular string, plural string) string {
+	if num == 1 {
+		return singular
+	}
+	return plural
 }
